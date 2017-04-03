976b4716d89fb4668e127e7eb0fef1009da3229e
oskar <oskar.hint@gmail.com>
2017-04-03 18:05:50 +0200

    Mongodb start

--- src/main/java/dk/dtu/engauge/GreetingController.java
+++ src/main/java/dk/dtu/engauge/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	
+	@Autowired
+	private CustomerRepository repository;
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping(path = "/api/sessions", method = RequestMethod.POST)
-	public String SessionUpload(@RequestBody String body) {
+	public String SessionUpload(@RequestBody JSONObject obj) {
 
-		System.out.println(body);
+		System.out.println(obj);
+		
+		
 		
 		return "ok";
 	}
