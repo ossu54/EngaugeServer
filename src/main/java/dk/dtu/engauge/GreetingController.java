6a857402b4fbc290e015b53182a3b11424eb4348
oskar <oskar.hint@gmail.com>
2017-04-03 16:42:40 +0200

    JSON endpoint

--- src/main/java/dk/dtu/engauge/GreetingController.java
+++ src/main/java/dk/dtu/engauge/GreetingController.java
@@ -2,7 +2,12 @@ package dk.dtu.engauge;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +17,14 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	
+	@RequestMapping(path = "/api/sessions", method = RequestMethod.POST)
+	public String SessionUpload(@RequestBody String body) {
+
+		System.out.println(body);
+		
+		return "ok";
+	}
+
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
